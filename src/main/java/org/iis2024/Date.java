64b114fbe5effb3a6c0799e3542173f9aee161b8
Rafa <rafanavarroortega@gmail.com>
2026-03-23 20:09:18 +0100

    new method added

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -20,6 +20,10 @@ public class Date {
   private final int month;
   private final int year;
 
+  public void methodAddedByOwner() {
+    System.out.println();
+  }
+
   /**
    * Retrieves the day component of this date.
    *
